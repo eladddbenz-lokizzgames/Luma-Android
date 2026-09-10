7f07da66cb9fe6198fb2eb604bcd1a0dcd00b214
eladddbenz-lokizzgames <eladddbenz@gmail.com>
2026-09-10 17:05:19 +0300

    Switch Android shell from Puter to anonymous AI Horde

--- app/src/main/java/com/luma/app/MainActivity.java
+++ app/src/main/java/com/luma/app/MainActivity.java
@@ -2,12 +2,8 @@ package com.luma.app;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.os.Message;
-import android.view.ViewGroup;
 import android.webkit.CookieManager;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -24,75 +20,26 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        web = createWebView();
-        setContentView(web);
-        loadApp();
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private WebView createWebView() {
-        WebView view = new WebView(this);
-        WebSettings s = view.getSettings();
+        web = new WebView(this);
+        WebSettings s = web.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
         s.setDatabaseEnabled(true);
         s.setLoadsImagesAutomatically(true);
         s.setMediaPlaybackRequiresUserGesture(false);
-        s.setSupportMultipleWindows(true);
-        s.setJavaScriptCanOpenWindowsAutomatically(true);
         s.setAllowContentAccess(true);
         s.setAllowFileAccess(true);
         s.setBuiltInZoomControls(false);
         s.setDisplayZoomControls(false);
-
-        CookieManager cookies = CookieManager.getInstance();
-        cookies.setAcceptCookie(true);
-        cookies.setAcceptThirdPartyCookies(view, true);
-
-        view.setWebViewClient(new WebViewClient());
-        view.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onCreateWindow(WebView source, boolean isDialog, boolean isUserGesture, Message resultMsg) {
-                final Dialog dialog = new Dialog(MainActivity.this);
-                final WebView popup = createPopupWebView(dialog);
-                dialog.setContentView(popup, new ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT));
-                dialog.show();
-
-                WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
-                transport.setWebView(popup);
-                resultMsg.sendToTarget();
-                return true;
-            }
-        });
-        return view;
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private WebView createPopupWebView(final Dialog dialog) {
-        WebView popup = new WebView(this);
-        WebSettings s = popup.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setSupportMultipleWindows(true);
-        s.setJavaScriptCanOpenWindowsAutomatically(true);
-        s.setLoadsImagesAutomatically(true);
-        CookieManager.getInstance().setAcceptThirdPartyCookies(popup, true);
-        popup.setWebViewClient(new WebViewClient());
-        popup.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onCloseWindow(WebView window) {
-                window.destroy();
-                if (dialog.isShowing()) dialog.dismiss();
-            }
-        });
-        return popup;
+        CookieManager.getInstance().setAcceptCookie(true);
+        web.setWebViewClient(new WebViewClient());
+        setContentView(web);
+        loadApp();
     }
 
     private void loadApp() {
         try {
-            InputStream in = getAssets().open("puter.html");
+            InputStream in = getAssets().open("horde.html");
             BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
             StringBuilder html = new StringBuilder();
             String line;
